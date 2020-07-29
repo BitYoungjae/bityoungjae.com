a5f3f77446be69af83dec33688ccb4dc87d1080a
Youngjae Joo <bityoungjae@gmail.com>
2020-07-30 00:05:20 +0900

    Next.js 신규 에러 처리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../components/styles/themes/default';
 import { GlobalStyle } from 'components/styles/GlobalStyle';
@@ -6,6 +7,12 @@ import { GlobalStyle } from 'components/styles/GlobalStyle';
 function PostStoreApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+        />
+      </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,10 +7,6 @@ export default class MyDocument extends Document {
     return (
       <html lang='ko'>
         <Head>
-          <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1, shrink-to-fit=no'
-          />
           <link rel='shortcut icon' href='/favicon.png' type='image/png' />
           <link
             href='https://fonts.googleapis.com/css2?family=Jua&family=Noto+Sans+KR:wght@400;700&family=Montserrat:wght@900&display=swap'
